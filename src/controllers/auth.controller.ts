de024ae5776273c786dc053ecf8f7071e7f5faf4
Maher Lawand <maherlawand@Ziads-MacMini-Pro.local>
2026-05-18 01:17:12 +0300

    fix logout controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -164,7 +164,11 @@ export async function logout(
   req: Request,
   res: Response
 ) {
-  res.clearCookie("token")
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  })
 
   return res.json({
     message: "Logged out",
